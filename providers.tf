f0b89ed810e64c394170743056c9303e08094c02
naresh9000 <92814032+naresh9000@users.noreply.github.com>
2023-12-11 12:32:44 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "AKIA34LV5646546546546546544464646SZHOPX66O5VE"
-  secret_key = "JOzqHOa5C/7E/B112vMUX544654654984531654645613546545616Ql8iZkHrHjk806UuWd6"
+  access_key = "<access-key>"
+  secret_key = "<secret-key>"
   region     = "ap-south-2"
 }
